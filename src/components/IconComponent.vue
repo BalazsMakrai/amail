a267a90fe3c9950d0e610647a559e768027fc50e
Balazs Makrai <b.makrai@gmail.com>
2023-05-30 17:28:11 +0200

    add navbar icons

---
+++ src/components/IconComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="IconComponent" class="hello">
+    <div
+      class="flex items-center justify-center rounded-full cursor-pointer w-10 h-10"
+      :class="[hoverColor]"
+      :data-tooltip-target="`tooltip-no-arrow-${iconString}`"
+      data-tooltip-placement="bottom"
+    >
+      <component :is="icon" :size="iconSize" :fillColor="iconColor" />
+    </div>
+    <div
+      :id="`tooltip-no-arrow-${iconString}`"
+      role="tooltip"
+      class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 delay-150 bg-gray-500 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+    >
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { toRefs, defineProps } from "vue";
+import MenuIcon from "vue-material-design-icons/Menu.vue";
+import MagnifyIcon from "vue-material-design-icons/Magnify.vue";
+import TuneIcon from "vue-material-design-icons/Tune.vue";
+import HelpIcon from "vue-material-design-icons/Help.vue";
+import CogOutlineIcon from "vue-material-design-icons/CogOutline.vue";
+import AppsIcon from "vue-material-design-icons/Apps.vue";
+import ArrowLeftIcon from "vue-material-design-icons/ArrowLeft.vue";
+import TrashCanOutlineIcon from "vue-material-design-icons/TrashCanOutline.vue";
+let icon = null;
+const props = defineProps({
+  iconString: String,
+  iconColor: String,
+  iconSize: Number,
+  text: String,
+  hoverColor: String,
+});
+const { iconString, iconColor, text, hoverColor } = toRefs(props);
+if (iconString.value === "menu") {
+  icon = MenuIcon;
+} else if (iconString.value === "magnify") {
+  icon = MagnifyIcon;
+} else if (iconString.value === "tune") {
+  icon = TuneIcon;
+} else if (iconString.value === "help") {
+  icon = HelpIcon;
+} else if (iconString.value === "cog") {
+  icon = CogOutlineIcon;
+} else if (iconString.value === "apps") {
+  icon = AppsIcon;
+} else if (iconString.value === "back") {
+  icon = ArrowLeftIcon;
+} else if (iconString.value === "trash") {
+  icon = TrashCanOutlineIcon;
+}
+</script>
---
+++ src/views/template/EmailView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="EmailView" class="bg-gray-50 h-screen">
+    <div class="flex items-center p-2">
+      <div class="logo-section flex justify-start items-center px-3.5">
+        <IconComponent
+          class="-ml-2 mr-2"
+          iconString="menu"
+          iconColor="#636363"
+          :iconSize="19"
+          hoverColor="hover:bg-gray-200"
+          text="Main menu"
+        />
+        <img src="../../../public/img/GmailLogo.png" width="110" />
+      </div>
+      <div class="flex w-full justify-between">
+        <div class="w-full bg-gray-200 flex items-center p-1 px-2 rounded-lg">
+          <IconComponent
+            iconString="magnify"
+            iconColor="#636363"
+            :iconSize="19"
+            hoverColor="hover:bg-gray-200"
+            text="Search"
+          /><input
+            placeholder="Search mail"
+            type="text"
+            class="w-full h-10 bg-gray-200 border-transparent border-none focus:ring-0 outline-none placeholder-gray-900"
+          />
+          <IconComponent
+            iconString="tune"
+            iconColor="#636363"
+            :iconSize="19"
+            hoverColor="hover:bg-gray-200"
+            text="Show search options"
+          />
+        </div>
+        <div class="flex w-32 justify-between items-center ml-6">
+          <IconComponent
+            iconString="cog"
+            iconColor="#636363"
+            :iconSize="19"
+            hoverColor="hover:bg-gray-200"
+            text="Settings"
+          /><IconComponent
+            iconString="apps"
+            iconColor="#636363"
+            :iconSize="19"
+            hoverColor="hover:bg-gray-200"
+            text="Google apps"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import IconComponent from "../../components/IconComponent.vue";
+</script>
+<style scoped>
+.logo-section {
+  min-width: 250px;
+}
+</style>
