9e71944715d9423c4d8762219cc2c2c28df2133f
BalazsMakrai <64318333+BalazsMakrai@users.noreply.github.com>
2023-06-15 18:58:01 +0200

    add google login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="LoginView" class="grid h-screen place-items-center">
+<div>
+    <img width="280" src="img/GmailLogin.png" >
+    <div class="flex justify-center">
+        <GoogleLogin :callback="callback"/>
+    </div>
+</div>
+    </div>
+</template>
+
+<script setup>
+const callback=(response)=>{
+    console.log(response);
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
